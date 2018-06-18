633d3006ab8710f5e58424347a507c44fe6403da
JiaruiMi <mjrpumc@outlook.com>
2018-06-18 23:27:42 +0200

    SCDE first 2 tutorial

--- SingleCell_RNA_seq_Analysis_template.R
+++ SingleCell_RNA_seq_Analysis_template.R
@@ -4322,6 +4322,8 @@ pheatmap::pheatmap(t(assigned$scores))
 # The SCDE package implements a set of statistical methods for analyzing single-cell RNA-seq data, including differential 
 # expression analysis (Kharchenko et al.) and pathway and geneset overdispersion analysis (Fan et al.) 
 # SCDE这个包集成了pagoda，其特点在于采用了多种统计学模型手段计算差异表达基因，并且可以作通路和基因集的过离散分析。
+# SCDE是第一个针对scRNA建立的差异基因表达分析方法，它使用了贝叶斯模型，并基于zero-inflated 负二项分布，最新的版本可以检测一群基因的
+# 的平均表达和dispersion，这个所谓的一群基因可能是一个pathway里面的gene。
 
 # SCDE基于的统计学假说是，我们的reads counts table是服从两个统计学分布的，一个是负二项分布（这是针对哪些可以被检测到，在
 # RNA逆转录为DNA后可以被扩增的那一部分转录本）和low-level Poisson分布（这是针对那些不能被检测到，背景信号，往往是不能很好
@@ -4476,7 +4478,226 @@ p.self.fail <- scde.failure.probability(models = o.ifm, counts = cd)
 
 #---------------------# Adjusted distance meaures #---------------------#
 
-## 好像有点问题，建议：http://hms-dbmi.github.io/scde/diffexp.html
+## 好像有点问题，链接：http://hms-dbmi.github.io/scde/diffexp.html
+require(parallel)
+p.self.fail <- scde.failure.probability(models = o.ifm, counts = cd)
+# simulate drop-outs
+# note: using 10 sampling rounds for illustration here. ~500 or more should be used.
+n.simulations <- 10; k <- 0.9;
+cell.names <- colnames(cd); names(cell.names) <- cell.names;
+dl <- mclapply(1:n.simulations,function(i) {
+  scd1 <- do.call(cbind,lapply(cell.names,function(nam) {
+    x <- cd[,nam];
+    # replace predicted drop outs with NAs
+    x[!as.logical(rbinom(length(x),1,1-p.self.fail[,nam]*k))] <- NA;
+    x;
+  }))
+  rownames(scd1) <- rownames(cd); 
+  # calculate correlation on the complete observation pairs
+  cor(log10(scd1+1),use="pairwise.complete.obs");
+}, mc.cores = 1)
+# calculate average distance across sampling rounds
+direct.dist <- as.dist(1-Reduce("+",dl)/length(dl))
+
+
+######################################### Pathway and Gene Set Overdispersion Analysis #########################################
+# 背景：
+# 我们使用SCDE里面的pagoda来刻画和描述单细胞群体中的转录异质性
+# 根据SCDE得到的细胞特异的error model用来预测基因表达变化的残差，并发现具有过离散的（statistical significant excess of 
+# coordinated variability）通路或者是基因集. Pagoda可以敏锐的发现已知的细胞亚群并发现新的细胞亚群和它们各自的独特功能。
+# Pagoda能够通过发现细胞之间表现为过离散的通路或者是基因集来发现转录的异质性。
+
+#---------------------# Preparing data #---------------------#
+# 我们仍然从read counts的矩阵开始，我们使用Pollen的内置数据集。
+data(pollen)
+# remove poor cells and genes
+cd <- clean.counts(pollen)
+# check the final dimensions of the read count matrix
+dim(cd)
+colnames(cd)
+
+# 接下来我们给不同的细胞group和sample source赋予颜色，这样方便与后面Pagoda得到的结果进行比较
+x <- gsub("^Hi_(.*)_.*", "\\1", colnames(cd))
+l2cols <- c("coral4", "olivedrab3", "skyblue2", "slateblue3")[as.integer(factor(x, 
+                                                                                levels = c("NPC", "GW16", "GW21", "GW21+3")))]
+
+
+#---------------------# Fitting error models #---------------------#
+# 接下来我们针对每个细胞计算error model。我们通过knn.error.model()来调用k-nearest neighbor方法。这个数据集的噪音比较高，因此我们对gene和细胞的过滤
+# 比较严格，最少的gene检测阈值min.count.threshold是2，需要至少5个细胞有这么高的表达量。
+# We're providing a rough guess to the complexity of the population, by fitting the error models based on 1/4 of most similar cells (i.e. guessing 
+# there might be ~4 subpopulations).
+
+require(parallel)
+# EVALUATION NOT NEEDED
+knn <- knn.error.models(cd, k = ncol(cd)/4, n.cores = 8, min.count.threshold = 2, min.nonfailed = 5, max.model.plots = 10)
+
+# 当然你也可以不必作上述计算，针对这个测试数据，已经有事先计算好的结果
+data(knn)
+knn
+## 上述这个fitting model会在当前文件夹下产生一个叫做cell.model.pdf的文件，根据max.model.plots参数可知我们展示了前10个细胞的fit model
+## 对照图去理解以下的这个英文段落：
+## The two scatter plots on the left show observed (in a given cell) vs. expected (from k similar cells) expression magnitudes for each gene that 
+## is being used for model fitting. The second (from the left) scatter plot shows genes belonging to the drop-out component in red. The black dashed 
+## lines show 95% confidence band for the amplified genes (the grey dashed lines show confidence band for an alternative constant-theta model). 
+## The third plot shows drop-out probability as a function of magnitude, and the fourth plot shows negative binomial theta local regression fit as a 
+## function of magnitude (for the amplified component).
+
+
+#---------------------# Normalizing variance #---------------------#
+# 这一步说白了就是找到HVG
+# In order to accurately quantify excess variance or overdispersion, we must normalize out expected levels of technical and intrinsic biological 
+# noise. Briefly, variance of the NB/Poisson mixture processes derived from the error modeling step are modeled as a chi-squared distribution 
+# using adjusted degrees of freedom and observation weights based on the drop-out probability of a given gene. Here, we normalize variance, 
+# trimming 3 most extreme cells and limiting maximum adjusted variance to 5.
+varinfo <- pagoda.varnorm(knn, counts = cd, trim = 3/ncol(cd), max.adj.var = 5, n.cores = 8, plot = TRUE)
+## 看懂这张图，请理解这句话
+## The plot on the left shows coefficient of variance squared (on log10 scale) as a function of expression magnitude (log10 FPM). The red line 
+## shows local regression model for the genome-wide average dependency. The plot on the right shows adjusted variance (derived based on chi-squared 
+## probability of observed/genomewide expected ratio for each gene, with degrees of freedom adjusted for each gene). The adjusted variance of 1 
+## means that a given gene exhibits as much variance as expected for a gene of such population average expression magnitude. Genes with high 
+## adjusted variance are overdispersed within the measured population and most likely show subpopulation-specific expression:
+
+# list top overdispersed genes
+sort(varinfo$arv, decreasing = TRUE)[1:10]
+
+
+#---------------------# Controlling for sequencing depth #---------------------#
+# Below we will control for the gene coverage (estimated as a number of genes with non-zero magnitude per cell) and normalize out that aspect of 
+# cell heterogeneity:
+varinfo <- pagoda.subtract.aspect(varinfo, colSums(cd[, rownames(knn)]>0))
+
+
+#---------------------# Evaluate overdispersion of pre-defined gene sets #---------------------#
+# In order to detect significant aspects of heterogeneity across the population of single cells, 'pagoda' identifies pathways and gene sets that 
+# exhibit statistically significant excess of coordinated variability. Specifically, for each gene set, we tested whether the amount of variance 
+# explained by the first principal component significantly exceed the background expectation. We can test both pre-defined gene sets as well as 'de 
+# novo' gene sets whose expression profiles are well-correlated within the given dataset.
+
+# For pre-defined gene sets, we'll use GO annotations. For the purposes of this vignette, in order to make calculations faster, we will only 
+# consider the first 100 GO terms plus a few that we care about. Additional tutorials on how to create and use your own gene sets can be found in 
+# a separate tutorial. 我们挑选前100个GO和我们感兴趣的GO来进行测试
+
+library(org.Hs.eg.db)
+# translate gene names to ids
+ids <- unlist(lapply(mget(rownames(cd), org.Hs.egALIAS2EG, ifnotfound = NA), function(x) x[1]))
+rids <- names(ids); names(rids) <- ids 
+# convert GO lists from ids to gene names
+gos.interest <- unique(c(ls(org.Hs.egGO2ALLEGS)[1:100],"GO:0022008","GO:0048699", "GO:0000280")) 
+go.env <- lapply(mget(gos.interest, org.Hs.egGO2ALLEGS), function(x) as.character(na.omit(rids[x]))) 
+go.env <- clean.gos(go.env) # remove GOs with too few or too many genes
+go.env <- list2env(go.env) # convert to an environment
+
+# Now, we can calculate weighted first principal component magnitudes for each GO gene set in the provided environment.
+pwpca <- pagoda.pathway.wPCA(varinfo, go.env, n.components = 1, n.cores = 8)
+
+# We can now evaluate the statistical significance of the observed overdispersion for each GO gene set.
+df <- pagoda.top.aspects(pwpca, return.table = TRUE, plot = TRUE, z.score = 1.96)
+## Each point on the plot shows the PC1 variance (lambda1) magnitude (normalized by set size) as a function of set size. 
+## The red lines show expected (solid) and 95% upper bound (dashed) magnitudes based on the Tracey-Widom model.
+head(df)
+## 1，The z column gives the Z-score of pathway over-dispersion relative to the genome-wide model (Z-score of 1.96 corresponds 
+## to P-value of 5%, etc.).
+## 2，"z.adj" column shows the Z-score adjusted for multiple hypothesis (using Benjamini-Hochberg correction).
+## 3，"score" gives observed/expected variance ratio
+## 4，"sh.z" and "adj.sh.z" columns give the raw and adjusted Z-scores of "pathway cohesion", which compares the observed 
+## PC1 magnitude to the magnitudes obtained when the observations for each gene are randomized with respect to cells. When such Z-score is high (e.g. for GO:0008009) then multiple genes within the pathway contribute to the coordinated pattern.
+
+
+#---------------------# Evaluate overdispersion of 'de novo' gene sets #---------------------#
+# We can also test 'de novo' gene sets whose expression profiles are well-correlated within the given dataset. The following procedure will 
+# determine 'de novo' gene clusters in the data, and build a background model for the expectation of the gene cluster weighted principal 
+# component magnitudes. Note the higher trim values for the clusters, as we want to avoid clusters that are formed by outlier cells.
+
+clpca <- pagoda.gene.clusters(varinfo, trim = 7.1/ncol(varinfo$mat), n.clusters = 50, n.cores = 8, plot = TRUE) # 非常耗时
+## The plot above shows background distribution of the first principal component (PC1) variance (lambda1) magnitude. The blue scatterplot on the left shows 
+## lambda1 magnitude vs. cluster size for clusters determined based on randomly-generated matrices of the same size. The black circles show top cluster 
+## in each simulation. The red lines show expected magnitude and 95% confidence interval based on Tracy-Widom distribution. The right plot shows extreme 
+## value distribution fit of residual cluster PC1 variance magnitude relative to the Gumbel (extreme value) distribution.
+
+
+# Now the set of top aspects can be recalculated taking these de novo gene clusters into account:
+df <- pagoda.top.aspects(pwpca, clpca, return.table = TRUE, plot = TRUE, z.score = 1.96)
+head(df)
+## The gene clusters and their corresponding model expected value and 95% upper bound are shown in green.
+
+#---------------------# Visualize significant aspects of heterogeneity #---------------------#
+# To view top heterogeneity aspects, we will first obtain information on all the significant aspects of transcriptional heterogeneity. We will 
+# also determine the overall cell clustering based on this full information:
+
+# get full info on the top aspects
+tam <- pagoda.top.aspects(pwpca, clpca, n.cells = NULL, z.score = qnorm(0.01/2, lower.tail = FALSE))
+# determine overall cell clustering
+hc <- pagoda.cluster.cells(tam, varinfo)
+
+# Next, we will reduce redundant aspects in two steps. First we will combine pathways that are driven by the same sets of genes:
+tamr <- pagoda.reduce.loading.redundancy(tam, pwpca, clpca)
+
+# In the second step we will combine aspects that show similar patterns (i.e. separate the same sets of cells). Here we will plot the cells 
+# using the overall cell clustering determined above:
+tamr2 <- pagoda.reduce.redundancy(tamr, distance.threshold = 0.9, plot = TRUE, cell.clustering = hc, labRow = NA, 
+                                  labCol = NA, box = TRUE, margins = c(0.5, 0.5), trim = 0)
+## In the plot above, the columns are cells, rows are different significant aspects, clustered by their similarity pattern.The green-to-orange 
+## color scheme shows low-to-high weighted PCA scores (aspect patterns), where generally orange indicates higher expression. Blocks of color 
+## on the left margin show which aspects have been combined by the command above. Here the number of resulting aspects is relatively small. "top" 
+## argument (i.e. top = 10) can be used to limit further analysis to top N aspects.
+
+# We will view the top aspects, clustering them by pattern similarity (note, to view aspects in the order of increasing lambda1 magnitude, 
+# use row.clustering = NA).
+col.cols <- rbind(groups = cutree(hc, 3))
+pagoda.view.aspects(tamr2, cell.clustering = hc, box = TRUE, labCol = NA, margins = c(0.5, 20), col.cols = rbind(l2cols))
+## While each row here represents a cluster of pathways, the row names are assigned to be the top overdispersed aspect in each cluster.
+
+# To interactively browse and explore the output, we can create a pagoda app:
+# compile a browsable app, showing top three clusters with the top color bar
+app <- make.pagoda.app(tamr2, tam, varinfo, go.env, pwpca, clpca, col.cols = col.cols, cell.clustering = hc, title = "NPCs")
+# show app in the browser (port 1468)
+show.app(app, "pollen", browse = TRUE, port = 1468)  # 看一下这个youtube tutorial
+## The pagoda app allows you to view the gene sets grouped within each aspect (row), as well as genes underlying the detected heterogeneity 
+## patterns. A screenshot of the app is provided below:
+
+
+# Similar views can be obtained in the R session itself. For instance, here we'll view top 10 genes associated with the top two pathways in 
+# the neurogenesis cluster: "neurogenesis" (GO:0022008) and "generation of neurons" (GO:0048699)
+pagoda.show.pathways(c("GO:0022008","GO:0048699"), varinfo, go.env, cell.clustering = hc, margins = c(1,5), 
+                     show.cell.dendrogram = TRUE, showRowLabels = TRUE, showPC = TRUE)
+
+
+# Adding 2D embedding
+## One can add a 2D embedding of the cells to aid visualization. The code below uses PAGODA's weighted Pearson correlation distance (that was 
+## used to derive hierarchical clustering) to generate a tSNE embedding of the cells:
+library(Rtsne);
+# recalculate clustering distance .. we'll need to specify return.details=T
+cell.clustering <- pagoda.cluster.cells(tam,varinfo,include.aspects=TRUE,verbose=TRUE,return.details=T)
+## fix the seed to ensure reproducible results
+set.seed(0); 
+tSNE.pagoda <- Rtsne(cell.clustering$distance,is_distance=T,initial_dims=100,perplexity=10)
+par(mfrow=c(1,1), mar = c(2.5,2.5,2.0,0.5), mgp = c(2,0.65,0), cex = 1.0);
+plot(tSNE.pagoda$Y,col=adjustcolor(col.cols,alpha=0.5),cex=1,pch=19,xlab="",ylab="")
+
+## The resulting embedding can be passed into the PAGODA app to visualize individual aspects, genes, etc. within this embedding:
+app <- make.pagoda.app(tamr2, tam, varinfo, go.env, pwpca, clpca, col.cols = col.cols, cell.clustering = hc, title = "NPCs", embedding = tSNE.pagoda$Y)
+# show app in the browser (port 1468)
+show.app(app, "pollen", browse = TRUE, port = 1468) 
+
+
+
+#---------------------# Controlling for undesired aspects of heterogeneity #---------------------#
+# Depending on the biological setting, certain dominant aspects of transcriptional heterogeneity may not be of interest. To explicitly control 
+# for these aspects of heterogeneity that are not of interest, we will use pagoda.subtract.aspect method that we've previously used to control 
+# for residual patterns associated with sequencing depth differences. Here, we illustrate how to control for the mitotic cell cycle pattern 
+# (GO:0000280 nuclear division and GO:0007067 mitotic nuclear division) which showed up as one of the four significant aspects in the analysis 
+# above.
+
+# get cell cycle signature and view the top genes
+cc.pattern <- pagoda.show.pathways(c("GO:0000280", "GO:0007067"), varinfo, go.env, show.cell.dendrogram = TRUE, cell.clustering = hc, showRowLabels = TRUE)
+# subtract the pattern
+varinfo.cc <- pagoda.subtract.aspect(varinfo, cc.pattern)
+## Now we can go through the same analysis as shown above, starting with the pagoda.pathway.wPCA() call, using varinfo.cc instead of varinfo, 
+## which will control for the cell cycle heterogeneity between the cells.
+
+
+
 
 
 
